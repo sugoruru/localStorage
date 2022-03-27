18d8a25993c3a565184f76d58c923a55dba2e2b9
くるみ <88281866+sugoruru@users.noreply.github.com>
2022-03-27 18:15:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function save() {
     localStorage.setItem("storage", document.getElementById("input").value);
+    alert("セーブしました");
 }
 
 function load() {
     document.getElementById("input").value = localStorage.getItem("storage");
+    alert("読み込み完了！");
 }
